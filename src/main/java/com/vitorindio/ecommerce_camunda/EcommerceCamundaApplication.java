0563d1a75a1d6c8a00a3a09342136990ee3016e7
VitorIndio <vitor.indio@owse.com.br>
2026-02-13 19:21:14 -0300

    chore: initial commit - Spring Boot + Camunda 8 project setup

---
+++ src/main/java/com/vitorindio/ecommerce_camunda/EcommerceCamundaApplication.java
@@ -0,0 +1,13 @@
+package com.vitorindio.ecommerce_camunda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceCamundaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceCamundaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vitorindio/ecommerce_camunda/EcommerceCamundaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vitorindio.ecommerce_camunda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceCamundaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
